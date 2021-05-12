8784fa4467e6ba0118c0551b186eba35989a54c2
Eddy Lardet <eddy.lardet@baanx.com>
2021-05-12 19:48:36 +0200

    remove redis

--- src/db.ts
+++ src/db.ts
@@ -1,18 +1,8 @@
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
-import { Tedis } from 'tedis';
 import logger from './utils/logger';
 
 export async function intializeDB(): Promise<void> {
   await createConnection();
   logger.info('Database successfully initialized');
-}
-
-export function initializeCache(port: number | undefined) : unknown {
-  const tedis = new Tedis({
-    host: '127.0.0.1',
-    port
-  });
-  logger.info('Redis cache successfully initialized');
-  return tedis;
 }
